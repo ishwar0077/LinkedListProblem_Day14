9c9f9fc26c31cae074941795e263afd76c40ca14
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-20 19:31:22 +0530

    Gretting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListPracticeProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Linked List Program");
+        }
+    }
+}
