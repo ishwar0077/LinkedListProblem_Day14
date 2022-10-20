65db54fb200381dd60c1587d00677824b809ccce
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-20 19:36:59 +0530

    Simple Implementation of Linked List

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Linked List Program");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
