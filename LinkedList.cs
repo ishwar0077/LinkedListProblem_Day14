bbd979b7d880de20872d6a0e4c00db4b2b6dcb13
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-20 20:36:03 +0530

    Ability to inset 30 between 56 and 70

--- LinkedList.cs
+++ LinkedList.cs
@@ -11,41 +11,53 @@ namespace LinkedListPracticeProblem
         Node head = null;
         public void Add(int data)
         {
-                Node node = new Node(data);
-                if (this.head == null)
-                {
-                    this.head = node;
-                }
-                else
-                {
-                    Node temp = head;
-                    while (temp.next != null)
-                    {
-                        temp = temp.next;
-                    }
-                    temp.next = node;
-                }
-                Console.WriteLine(node.data + ":Appended SuccessFully");
-           
-        }
-            public void Display()
+            Node node = new Node(data);
+            if (this.head == null)
             {
-                Node temp = this.head;
-                if (temp == null)
-                {
-                    Console.WriteLine("LinkedList Is Empty");
-                }
-                else
-                {
-                    Console.WriteLine("Values In LinkedList Are:");
-                    while (temp != null)
-                    {
-                        Console.Write(temp.data + " ");
-                        temp = temp.next;
-                    }
-                }
-
+                this.head = node;
             }
-        
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + ":Added SuccessFully");
+        }
+        public void insertbetween(int value, int data)
+        {
+            Node node = head;
+            while (node.data != value)
+            {
+                node = node.next;
+            }
+            Node node1 = new Node(data);
+            Node temp;
+            temp = node.next;
+            node.next = node1;
+            node1.next = temp;
+
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList Is Empty");
+            }
+            else
+            {
+                Console.WriteLine("Values In LinkedList Are:");
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@
         {
             Console.WriteLine("Welcome To Linked List Program");
 
-            
+            Console.WriteLine("problem : Ability to insert 30 between 56 and 70");
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.Display();
+            Console.WriteLine("Inserting 30 Value Between 56 And 70:");
+            list.insertbetween(56, 30);
             list.Display();
         }
     }
